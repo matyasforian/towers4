c7507831d64584eb70601a501515208ea49bc227
Matyas Forian Szabo <idontgivemy@email.com>
2017-03-30 01:36:55 +0200

    final

--- tower4.py
+++ tower4.py
@@ -4,15 +4,17 @@ class Tile:
         self.x = x
         self.y = y
         self.value = None
-        self.guesses = [num + 1 for num in range(NUM_TILES)]
+        self.guesses = {1, 2, 3, 4}
 
     def set_value(self, val):
         if self.value is not None and self.value != val:
             print("WARN: Trying to set existing value from " + str(self.value) + " to " + str(val))
         self.value = val
-        self.guesses = []
+        self.guesses = set()
 
     def __str__(self):
+        if self.value is None:
+            return "."
         return str(self.value)
 
     def __repr__(self):
@@ -20,35 +22,79 @@ class Tile:
 
 
 # clues are clockwise: 0..3 4..7 8..11 12..15
-def solve_puzzle(the_clues):
-    global clues
-    clues = the_clues
+def solve_puzzle(clues):
+    print(clues)
+    board = [Tile(num % NUM_TILES, num // NUM_TILES) for num in range(NUM_TILES * NUM_TILES)]
     solution = [[] for num in range(NUM_TILES)]  # list of columns
 
-    # for i in range(16):
-    #    board[i].set_value(i)
-    # for i in range(16):
-    #    print(get_line(i))
-    # print_board()
-    fill_trivial_cases()
-    print_board()
-    fill_last_element()
-    fill_last_occurence()
-    print_board()
-    # try to find numbers that are 3 times in the board and write in the 4th
-    # eliminate possibilities based on LUTs
+    fill_trivial_cases(board, clues)
+
+    for i in range(10):
+        print_board(board)
+        fill_last_element(board)
+        fill_last_occurence(board)
+        eliminate_LUT(board, clues)
+        eliminate_guess(board)
+        fill_single_guess(board)
+        if is_done(board):
+            break
+
+    print_board(board)
 
     solution = [[] for num in range(NUM_TILES)]
     for i in range(NUM_TILES):
         for k in range(NUM_TILES):
-            solution[k].append(board[k * NUM_TILES + i])
+            solution[k].append(board[k * NUM_TILES + i].value)
     for i in range(NUM_TILES):
         solution[i] = tuple(solution[i])
-    solution = tuple(solution)
-    return solution
+    solution_tup = tuple(solution)
+    return solution_tup
 
 
-def get_line(index):  # TODO make this generic
+def fill_single_guess(board):
+    for i in range(NUM_TILES * 2):
+        occurences = [0 for num in range(NUM_TILES)]
+        line = get_line(board, i)
+        for tile in line:
+            if tile.value is not None:
+                occurences[tile.value - 1] = 999
+            else:
+                for guess in tile.guesses:
+                    occurences[guess - 1] = occurences[guess - 1] + 1
+        for k in range(len(occurences)):
+            if occurences[k] == 1:
+                for tile in line:
+                    if k + 1 in tile.guesses:
+                        tile.set_value(k + 1)
+                        # this is not optimized. it should update the guesses and continue to check stuff
+                        return
+
+
+def eliminate_guess(board):
+    for i in range(NUM_TILES * 2):
+        line = get_line(board, i)
+        to_elim = []
+        for tile in line:
+            if tile.value is not None:
+                to_elim.append(tile.value)
+        for tile in line:
+            if tile.value is None:
+                for elim in to_elim:
+                    tile.guesses.discard(elim)
+                if len(tile.guesses) == 1:
+                    tile.set_value(tile.guesses.pop())
+                    break
+
+
+def is_done(board):
+    left = NUM_TILES * NUM_TILES
+    for tile in board:
+        if tile.value is not None:
+            left = left - 1
+    return left == 0
+
+
+def get_line(board, index):  # TODO make this generic
     ret = []
     for k in range(NUM_TILES):
         if index < 4:
@@ -62,10 +108,43 @@ def get_line(index):  # TODO make this generic
     return ret
 
 
+def eliminate_LUT(board, clues):
+    for i in range(NUM_TILES * 4):
+        if clues[i] == 2:
+            line = get_line(board, i)
+            match_line(LUT2, line)
+        if clues[i] == 3:
+            line = get_line(board, i)
+            match_line(LUT3, line)
+
+
+def match_line(lut, line):
+    all_sols = []
+    for possible_sol in lut:
+        good_sol = True
+        for i in range(NUM_TILES):
+            if line[i].value is not None and line[i].value != possible_sol[i]:
+                good_sol = False
+                break
+        if good_sol:
+            all_sols.append(possible_sol)
+
+    ret = []
+    for k in range(NUM_TILES):
+        sols = set()
+        for sol in all_sols:
+            sols.add(sol[k])
+        ret.append(sols)
+    for m in range(NUM_TILES):
+        line[m].guesses.intersection_update(ret[m])
+        if len(line[m].guesses) == 1:
+            line[m].set_value(line[m].guesses.pop())
+
+
 # fills a line if one element is missing
-def fill_last_element():
+def fill_last_element(board):
     for i in range(NUM_TILES * 2):
-        line = get_line(i)
+        line = get_line(board, i)
         found = [False for num in range(NUM_TILES)]
         for tile in line:
             if tile.value is not None:
@@ -74,11 +153,11 @@ def fill_last_element():
             to_fill = found.index(False)
             for tile in line:
                 if tile.value is None:
-                    tile.value = to_fill + 1
+                    tile.set_value(to_fill + 1)
                     break
 
 
-def fill_last_occurence():
+def fill_last_occurence(board):
     for i in range(1, NUM_TILES + 1):
         h_missing = [num for num in range(NUM_TILES)]
         v_missing = [num for num in range(NUM_TILES)]
@@ -91,9 +170,9 @@ def fill_last_occurence():
 
 
 # fills if clues are 1 or NUM_TILES
-def fill_trivial_cases():
+def fill_trivial_cases(board, clues):
     for i in range(len(clues)):
-        line = get_line(i)
+        line = get_line(board, i)
         if clues[i] == 1:  # line begins with NUM_TILES
             line[0].set_value(NUM_TILES)
         elif clues[i] == NUM_TILES:  # line is 1,2,3,..
@@ -101,20 +180,34 @@ def fill_trivial_cases():
                 line[k].set_value(k + 1)
 
 
-def print_board():
+def print_board(board):
     for i in range(NUM_TILES):
+        st = ""
         for j in range(NUM_TILES):
-            print(" " + str(board[i * NUM_TILES + j]), end='')
-        print()
-    print()
+            st = st + " " + str(board[i * NUM_TILES + j])
+        print(st)
+    print("  ")
 
-LUT3 = ()
+LUT2 = ((1, 4, 2, 3),
+        (1, 4, 3, 2),
+        (2, 1, 4, 3),
+        (2, 4, 1, 3),
+        (2, 4, 3, 1),
+        (3, 4, 1, 2),
+        (3, 4, 2, 1),
+        (3, 1, 4, 2),
+        (3, 2, 4, 1),
+        (3, 1, 2, 4),
+        (3, 2, 1, 4))
+
+LUT3 = ((1, 2, 4, 3),
+        (1, 3, 4, 2),
+        (1, 3, 2, 4),
+        (2, 1, 3, 4),
+        (2, 3, 1, 4),
+        (2, 3, 4, 1))
 
 NUM_TILES = 4
-board = [Tile(num % NUM_TILES, num // NUM_TILES) for num in range(NUM_TILES * NUM_TILES)]
-# board[0].set_value(3)
-# board[7].set_value(3)
-# board[10].set_value(3)
-clues = None
-# solve_puzzle((0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0))
-solve_puzzle((2,2,1,3, 2,2,3,1, 1,2,2,3, 3,2,1,3))
+
+# solve_puzzle((2, 2, 1, 3,  2, 2, 3, 1,  1, 2, 2, 3,  3, 2, 1, 3))
+solve_puzzle((0, 2, 0, 0,  0, 3, 0, 0,  0, 1, 0, 0,  0, 0, 1, 2))
